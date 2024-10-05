9f9d0e04ab5adf48e49f6315588881d871fdf059
Rishi jangir <rishijangirjr@gmail.com>
2024-10-05 14:11:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
  const soundItem = document.querySelector("#itemImg")
-
- soundItem.addEventListener("click",function(e){
-    if(e.target.nodeName==="IMG"){
-         playMugic(e.target.id)
-    }
-  }) 
+ const crash = document.querySelector("#crashmp3")
+ const snare = document.querySelector("#snaremp3")
+ const kick = document.querySelector("#kickmp3")
+ const tom = document.querySelector("#tommp3")
  
-   function playMugic(id){
-     let audio = new Audio(`${id}.mp3`)
-     audio.play()
-   }
+soundItem.addEventListener("keyup",function(e){
+      if(e.key==="a" ){
+        crash.play()  
+      }
+      else if(e.key==="s"){
+        snare.play()  
+      }
+      else if(e.key==="d"){
+        kick.play()  
+      }
+      else if(e.key==="f"){
+        tom.play()  
+      }
+      
+     
+    }) 
+    soundItem.tabIndex=0
+    soundItem.focus()
+    
+    
+    soundItem.addEventListener("click",function(e){
+      if(e.target.nodeName==="IMG"){
+
+        if(e.target.id==="crash"){
+          crash.play()        
+      }
+    else if(e.target.id==="kick"){
+      kick.play()        
+    }
+    else if(e.target.id==="snare"){
+      snare.play()        
+    }
+    else if(e.target.id==="tom"){
+    tom.play()        
+  }
+  }
+  
+}) 
+
+
+
+
+
+
+
+
+ // soundItem.addEventListener("click",function(e){
+ //    if(e.target.nodeName==="IMG"){
+ //         playMugic(e.target.id)
+ //    }
+ //  }) 
+ 
+ //   function playMugic(id){
+ //     let audio = new Audio(`${id}.mp3`)
+ //     audio.play()
+ //   }
    
 
 
